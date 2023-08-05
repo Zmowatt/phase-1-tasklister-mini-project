c26e9cabb731aec7553b441422c6a78215d5ea99
Zachary Mowatt <zamowatt@gmail.com>
2023-08-05 05:21:34 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+//const { documentBaseURL } = require("jsdom/lib/jsdom/living/helpers/document-base-url");
+// const newTaskDescription = documentBaseURL.getElementById("new-task-description");
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleToDo(e.target['new-task-description'].value);
+  })
 });
+
+
+function handleToDo(todo){
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.textContent = ' Finito! '
+  p.textContent = ` ${todo} `;
+  document.getElementById('tasks').appendChild(p);
+  p.appendChild(btn).addEventListener('click', (e) => {
+    e.preventDefault();
+    p.remove();
+  });
+};
