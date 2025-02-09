1acdc8c2698767560b37cd85b105b4e0a94ff600
Zachary Mowatt <zamowatt@gmail.com>
2025-02-08 20:35:22 -0500

    Lab complete

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 //const { documentBaseURL } = require("jsdom/lib/jsdom/living/helpers/document-base-url");
 // const newTaskDescription = documentBaseURL.getElementById("new-task-description");
 
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('form').addEventListener('submit', (e) => {
+document.addEventListener('DOMContentLoaded', function(){
+  document.querySelector('form').addEventListener('submit', (e) =>  {
     e.preventDefault();
-    handleToDo(e.target['new-task-description'].value);
+    addToDo(e.target['new-task-description'].value);
   })
-});
 
+  function addToDo(todo){
+    const p = document.createElement('p');
+    const btn = document.createElement('button');
+    
+    p.textContent = ` ${todo} `;
+    btn.textContent = 'Deletrius!';
 
-function handleToDo(todo){
-  let p = document.createElement('p');
-  let btn = document.createElement('button');
-  btn.textContent = ' Finito! '
-  p.textContent = ` ${todo} `;
-  document.getElementById('tasks').appendChild(p);
-  p.appendChild(btn).addEventListener('click', (e) => {
-    e.preventDefault();
-    p.remove();
-  });
-};
+    document.getElementById('tasks').appendChild(p);
+    p.appendChild(btn).addEventListener('click', function(e) {
+      e.preventDefault();
+      p.remove();
+    })  
+  }
+})
